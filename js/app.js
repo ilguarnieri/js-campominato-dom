91ec9fe956a70a6a274e26f203ba41fadde02925
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-02-17 09:37:09 +0100

    sistemazione js

--- js/app.js
+++ js/app.js
@@ -1,90 +1,34 @@
 
 const btn_play = document.querySelector('.btn_play');
+btn_play.addEventListener('click', startGame);
 
-btn_play.addEventListener('click',() => {
-
-    const select_difficulty = document.getElementById('select_difficulty').value;
-
-    //nessuna difficoltà    
-    if(select_difficulty === ''){
-        alert('ATTENZIONE\nSeleziona una difficoltà di gioco!');
-        location.reload();
-        return false;
-    }
-
+function startGame(){
+    //reset grid
     resetGrid();
-    gridGenerator(select_difficulty);
-})
+    console.clear();
 
+    //modalità gioco
+    const difficulty = modeGame();
 
-function gridGenerator (difficulty){
-
-    const gridElement = document.querySelector('.grid');
-    //impostazione border e padding color grid
-    gridElement.style.border = '0';
-    gridElement.style.padding = '0';
-    //aggiunta animazione grid
-    gridElement.classList.add('animation_grid');
-    let colonne_righe;
-
-    //impostazione colonne-righe
-    switch(difficulty){
-
-        case '1':
-            colonne_righe = 10;
-            break;
-
-        case '2':
-            colonne_righe = 9;
-            break;
-
-        case '3':
-            colonne_righe = 7;
-            break;
-    }
-
-    //totale celle
-    const square_tot = Math.pow(colonne_righe, 2);
-
-    const bomb = [];
-
-    while(bomb.length < 16){
-        const numbBomb = getRandom(1, square_tot);
-        if(!bomb.includes(numbBomb)){
-            bomb.push(numbBomb);
-        }
-    }
-
-    //creazione deglle celle
-    for(let i = 1; i <= square_tot; i++){
-
-        const square = document.createElement('div');
-        //aggiunta classe cella
-        square.classList.add('square');
-        //impostazioni dimensione cella
-        square.style.width = `calc(100% / ${colonne_righe})`;
-
-        if(bomb.includes(i)){
-            square.append('**');
-        }else{
-            square.append(i);
-        }
-
-        gridElement.append(square);
-    }
-
-
-
-    
-    gridElement.addEventListener('click', gridCallBack);
-    console.log(bomb);
+    //generazione grid
+    generateGrid(difficulty);
 }
 
 
-//funzione reset griglia
-const resetGrid = () =>{
+//reset contenuto grid
+function resetGrid(){
     const gridElement = document.querySelector('.grid');
     gridElement.innerHTML = '';
+    //impostazione border padding grid
+    gridElement.style.border = '0';
+    gridElement.style.padding = '0';
+}
+
+
+//modalità gioco
+function modeGame(){
+    const select_difficulty = document.getElementById('select_difficulty').value;
+    return parseInt(select_difficulty);
 }
 
 
@@ -94,16 +38,70 @@ function getRandom(min, max){
 }
 
 
+//generazione grid
+function generateGrid(difficulty){
+    const gridElement = document.querySelector('.grid');
+    let colums_rows;
 
+    //impostazione colonne-righe
+    switch(difficulty){
 
+        case 1:
+            //easy
+            colums_rows = 10;
+            break;
 
+        case 2:
+            //hard
+            colums_rows = 9;
+            break;
 
+        case 3:
+            //crazy
+            colums_rows = 7;
+            break;
+    }
+    //totale celle
+    const square_tot = Math.pow(colums_rows, 2);
 
+    //generazione bombe
+    const bombs = [];
+    while(bombs.length < 16){
+        const numbBomb = getRandom(1, square_tot);
+        if(!bombs.includes(numbBomb)){
+            bombs.push(numbBomb);
+        }
+    }
 
-function gridCallBack(event){
-    console.log(event.target);
-    const element = event.target.closest('.square');
-    console.log(element)
-    element.classList.add('selected');
+    //ordinamento array
+    bombs.sort((a, b) => a - b);
+    console.log(bombs);
+
+    //creazione deglle celle
+    for(let i = 1; i <= square_tot; i++){
+
+        const square = document.createElement('div');
+        //aggiunta classe cella
+        square.classList.add('square');
+        //impostazioni dimensione cella
+        square.style.width = `calc(100% / ${colums_rows})`;
+        //inserimento numero cella
+        square.dataset.number = i;
+        
+        gridElement.append(square);        
+    }
+       
+
+    function gridCallBack(event){
+        const element = event.target.closest('.square');
+        const numberSquare = parseInt(element.dataset.number);
+
+        if(bombs.includes(numberSquare)){
+            element.classList.add('red');
+        }else{
+            element.classList.add('selected');
+        }
+    }
+
+    gridElement.addEventListener('click', gridCallBack);
 }
-
